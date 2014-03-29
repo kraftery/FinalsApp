d96699aeeb58482dcf49209b081b0f701eb3d462
Kieran Raftery <kraftery7@gmail.com>
2014-03-29 16:33:02 -0400

    changed UI

--- FinalsApp/RootViewController.m
+++ FinalsApp/RootViewController.m
@@ -7,15 +7,11 @@
 //
 //test
 #import "RootViewController.h"
-#import "PageOneViewController.h"
-#import "PageTwoViewController.h"
 
 @interface RootViewController ()
 @end
 
 @implementation RootViewController
-
-@synthesize pageOne, pageTwo;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -29,61 +25,15 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    index = 0;
-    pageOne = [[PageOneViewController alloc] init];
-    pageTwo = [[PageTwoViewController alloc] init];
-
-    // Configure the page view controller and add it as a child view controller.
-    self.pageViewController = [[UIPageViewController alloc] initWithTransitionStyle:UIPageViewControllerTransitionStyleScroll navigationOrientation:UIPageViewControllerNavigationOrientationHorizontal options:nil];
-    self.pageViewController.delegate = self;
-    
-    NSArray *viewControllers = @[pageOne];
-    [self.pageViewController setViewControllers:viewControllers direction:UIPageViewControllerNavigationDirectionForward animated:NO completion:nil];
-    
-    self.pageViewController.dataSource = self;
-    
-    [self addChildViewController:self.pageViewController];
-    [self.view addSubview:self.pageViewController.view];
-    
-    // Set the page view controller's bounds using an inset rect so that self's view is visible around the edges of the pages.
-    CGRect pageViewRect = self.view.bounds;
-    self.pageViewController.view.frame = pageViewRect;
-    
-    [self.pageViewController didMoveToParentViewController:self];
-    
-    // Add the page view controller's gesture recognizers to the root view controller's view so that the gestures are started more easily.
-    self.view.gestureRecognizers = self.pageViewController.gestureRecognizers;
+    self.scrollView.contentSize = self.myView.bounds.size;
+    [self.scrollView addSubview:self.myView];
+    [self.scrollView flashScrollIndicators];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-#pragma mark - Page View Controller Data Source
-
-- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerBeforeViewController:(UIViewController *)viewController
-{
-    if (index == 0) {
-        return nil;
-    }
-    else {
-        return pageOne;
-    }
-}
-
-- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerAfterViewController:(UIViewController *)viewController
-{
-    if (index == 0) {
-        index = 1;
-        return pageTwo;
-    }
-    else {
-        index = 0;
-        return nil;
-    }
 }
 
 @end
