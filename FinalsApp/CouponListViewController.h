08d1dd32c3b0e5fdba7097b08ca0669446b30c61
Bori <admin@Admins-Mac.local>
2014-04-03 19:20:24 +0400

    added the table view for the lists of restaurants

---
+++ FinalsApp/CouponListViewController.h
@@ -0,0 +1,13 @@
+//
+//  CouponListViewController.h
+//  FinalsApp
+//
+//  Created by Bori on 4/3/14.
+//  Copyright (c) 2014 Kieran Raftery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CouponListViewController : UIViewController
+
+@end
