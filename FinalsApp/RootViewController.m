723b3cb5fe9cbf2749271568d621a0d08479d6d9
David LoBosco <davidlobosco@gmail.com>
2014-04-11 19:26:52 -0400

    almost done with nav

--- FinalsApp/RootViewController.m
+++ FinalsApp/RootViewController.m
@@ -46,9 +46,21 @@
     self.scrollView.contentSize = self.myView.bounds.size;
     [self.scrollView addSubview:self.myView];
     [self.scrollView flashScrollIndicators];
+    //self.title = @"UMD Finals App";
 }
 
-
+-(void)viewDidAppear:(BOOL)animated{
+    
+    
+}
+-(void)viewWillAppear:(BOOL)animated{
+    [self.header setAlpha:(0.0f)];
+    [self.navigationController setNavigationBarHidden:YES animated:NO];
+    [self.header setAlpha:(1.0f)];
+}
+-(void) viewWillDisappear:(BOOL)animated{
+    [self.navigationController setNavigationBarHidden:NO animated:NO];
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
